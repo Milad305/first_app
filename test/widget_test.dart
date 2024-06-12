9a072d5fdc1df44023390770c65c555d1877c474
Milad305 <58796025+Milad305@users.noreply.github.com>
2024-06-12 12:14:23 +0330

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -28,3 +28,46 @@ void main() {
     expect(find.text('1'), findsOneWidget);
   });
 }
+
+
+
+import React from 'react';
+
+const ColumnOfItems = ({ items }) => {
+  return (
+    <div>
+      {items.map((item, index) => (
+        <div key={index} className="mb-3">
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const AdditionalComponent = () => {
+  return (
+    <div className="ml-3">
+      <h2>Additional Component</h2>
+      {/* Add your additional component content here */}
+    </div>
+  );
+};
+
+const ParentComponent = () => {
+  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+
+  return (
+    <div className="row">
+      <div className="col-md-8">
+        <h1>Column of Items</h1>
+        <ColumnOfItems items={items} />
+      </div>
+      <div className="col-md-4">
+        <AdditionalComponent />
+      </div>
+    </div>
+  );
+};
+
+export default ParentComponent;
